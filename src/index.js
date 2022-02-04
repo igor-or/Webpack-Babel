b107bf1755407d6de7dbbe67b1b0d4a958577df7
ihor_sklad_pc <igor89.or@gmail.com>
2022-02-04 15:43:20 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { styleBody, addTitle, contact } from './dom';
+import users, { getPremUsers } from './data';
+
+const premUsers = getPremUsers(users);
+console.log(users, premUsers);
+
+console.log('test');
+
+// console.log('index file');
+
+// addTitle('test');
+// styleBody();
+
+// console.log(contact)
